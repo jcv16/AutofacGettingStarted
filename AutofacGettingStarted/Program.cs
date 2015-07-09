da146f9b0a5750c234d659597275b8c51ced1a0c
Jeffrey Valiao <jcv16@uclive.ac.nz>
2015-07-09 18:30:58 +1000

    Committed some changes after the getting started is done. Next commits will be for further experiments.

--- AutofacGettingStarted/Program.cs
+++ AutofacGettingStarted/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,27 @@ using System.Threading.Tasks;
 
 namespace AutofacGettingStarted
 {
-    class Program
+    class Program                   
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            WriteDate();
+        }
+
+        private static void WriteDate()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
         }
     }
 }
